36122e82a59ad4a7cfe63dff729c7c336a5251b7
KanthRao <KanthRao@KanthRao-PC>
2018-10-06 08:41:23 +0530

    both should display

---
+++ src/Githubimport.java
@@ -0,0 +1,7 @@
+
+public class Githubimport {
+	public static void main(String[] args) {
+		System.out.println("right click on class go to team select committ and select a class and click on next and commit and push");
+	}
+
+}
